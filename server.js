a3f35a4c2d8d87fe54bb0bc302b950d92b8e3bfa
Zvon Sri <zvonsri@gmail.com>
2023-08-25 01:06:07 -0400

    .

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const jsonServer = require('json-server');
 const dotenv = require('dotenv');
 dotenv.config();
+const PORT = process.env.PORT || 8080
+
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const PORT = process.env.PORT || 8080
-console.log(`ENV PORT: ${process.env.PORT}`);
+
 server.use(middlewares);
 server.use(router);
+
 server.listen(PORT, () => {
     console.log(`JSON Server is running at ${PORT} port`);
 })
